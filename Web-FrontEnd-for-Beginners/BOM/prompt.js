1c659361a81dd2c573b7dd05e9da6e9c00bbde59
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-03-10 08:00:30 +0700

    BOM: prompt

---
+++ Web-FrontEnd-for-Beginners/BOM/prompt.js
@@ -0,0 +1,12 @@
+// Silakan aktifkan kode di bawah ini dengan hapus komentar untuk melihat hasilnya
+let pesanInput = prompt('Silakan masukkan pesan yang kamu suka!');
+console.log(pesanInput);
+// parse atau cast tipe data
+let pesanInputNumber = Number(prompt('Masukkan angka sesukamu...'));
+
+// let pesanInputConvertedToNumber = Number(pesanInput);
+// console.log(`Tipe data dari \`pesanInputConvertedToNumber\` adalah ${typeof pesanInputConvertedToNumber}`);
+
+// Memberikan Nilai Default untuk prompt
+let name = prompt('Silakan masukkan nama Anda!', 'John Doe');
+console.log(name);
