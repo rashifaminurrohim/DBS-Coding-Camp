40559c9e1721d665108d774f7d27575344d4e75b
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-03-10 08:08:33 +0700

    BOM: console

---
+++ Web-FrontEnd-for-Beginners/BOM/console.js
@@ -0,0 +1,5 @@
+// Silakan aktifkan kode di bawah ini dengan hapus komentar untuk melihat hasilnya
+console.log('Ini adalah console log');
+console.info('Ini adalah console info');
+console.warn('Ini adalah console warn');
+console.error('Ini adalah console error');
