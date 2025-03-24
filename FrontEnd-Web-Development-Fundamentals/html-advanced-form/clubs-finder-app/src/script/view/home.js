ca83a6ed54d1b38a3e9591b850e54eeb53cf7816
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-03-24 14:02:17 +0700

    Study Case: Clubs finder app (Advance HTML form)

--- FrontEnd-Web-Development-Fundamentals/html-advanced-form/clubs-finder-app/src/script/view/home.js
+++ FrontEnd-Web-Development-Fundamentals/html-advanced-form/clubs-finder-app/src/script/view/home.js
@@ -2,19 +2,28 @@ import Utils from '../utils.js';
 import Clubs from '../data/local/clubs.js';
 
 const home = () => {
+  const searchFormElement = document.querySelector('#searchForm');
+
   const clubListContainerElement = document.querySelector('#clubListContainer');
   const clubLoadingElement = clubListContainerElement.querySelector('.search-loading');
   const clubListElement = clubListContainerElement.querySelector('.club-list');
   const listElement = clubListElement.querySelector('.list');
 
-  const showSportClub = () => {
+  const showSportClub = (query) => {
     showLoading();
 
-    const result = Clubs.getAll();
+    const result = Clubs.searchClub(query);
     displayResult(result);
 
     showClubList();
   };
+
+  const onSearchHandler = (event) => {
+    event.preventDefault();
+
+    const query = event.target.elements.name.value;
+    showSportClub(query);
+  }
 
   const displayResult = (clubs) => {
     const clubItems = clubs.map((club) => {
@@ -54,7 +63,7 @@ const home = () => {
     Utils.showElement(clubListElement);
   };
 
-  showSportClub();
+  searchFormElement.addEventListener('submit', onSearchHandler);
 };
 
 export default home;
