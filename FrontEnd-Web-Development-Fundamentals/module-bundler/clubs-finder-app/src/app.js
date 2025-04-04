1c5f04d5ec7411d2229d947b8a495d3cb4a64f48
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-04-04 14:52:08 +0700

    Module Bundler: Implement webpack to clubs finder app

--- FrontEnd-Web-Development-Fundamentals/module-bundler/clubs-finder-app/src/app.js
+++ FrontEnd-Web-Development-Fundamentals/module-bundler/clubs-finder-app/src/app.js
@@ -1,4 +1,5 @@
-import './script/components/index.js'
+import './styles/style.css';
+import './script/components/index.js';
 import home from './script/view/home.js';
 
 document.addEventListener('DOMContentLoaded', () => {
