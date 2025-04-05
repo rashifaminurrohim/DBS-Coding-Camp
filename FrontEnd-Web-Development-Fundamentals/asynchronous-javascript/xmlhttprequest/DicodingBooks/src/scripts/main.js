2b69deb9434a418251b67fcd4dde47c8e2c81d62
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-04-05 18:03:08 +0700

    Asynchronous Javascript: XmlHttpRequest dicoding books done

--- FrontEnd-Web-Development-Fundamentals/asynchronous-javascript/xmlhttprequest/DicodingBooks/src/scripts/main.js
+++ FrontEnd-Web-Development-Fundamentals/asynchronous-javascript/xmlhttprequest/DicodingBooks/src/scripts/main.js
@@ -1,27 +1,120 @@
 function main() {
+  const baseUrl = 'https://books-api.dicoding.dev';
 
   const getBook = () => {
     // tuliskan kode di sini!
+    // membuat instance dari XMLHttpRequest
+    const xhr = new XMLHttpRequest();
+
+    // menetapkan callback jika response sukses dan error
+    xhr.onload = function () {
+      const responseJson = JSON.parse(this.responseText);
+
+      if (responseJson.error) {
+        showResponseMessage(responseJson.message);
+      } else {
+        renderAllBooks(responseJson.books);
+      }
+    };
+
+    xhr.onerror = function () {
+      showResponseMessage();
+    }
+
+    // membuat GET request dan menetapkan target URL
+    xhr.open('GET', `${baseUrl}/list`);
+
+    // mengirimkan request
+    xhr.send();
   };
 
 
   const insertBook = (book) => {
     // tuliskan kode di sini!
+    // membuat instance dari XMLHttpRequest
+    const xhr = new XMLHttpRequest();
+
+    // menetapkan callback jika response sukses dan error
+    xhr.onload = function () {
+      const responseJson = JSON.parse(this.responseText);
+      showResponseMessage(responseJson.message);
+      getBook();
+    };
+
+    xhr.onerror = function () {
+      showResponseMessage();
+    }
+
+    // membuat POST request dan menetapkan target URL
+    xhr.open('POST', `${baseUrl}/add`);
+
+    // menetapkan properti Content-Type dan X-Auth-Token pada Header request
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('X-Auth-Token', '12345');
+
+    // mengirimkan request dan menyisipkan JSON.stringify(book) pada body request
+    xhr.send(JSON.stringify(book));
   };
 
   const updateBook = (book) => {
     // tuliskan kode di sini!
+    // Membuat instance dari XMLHttpRequest
+    const xhr = new XMLHttpRequest();
+
+    // Menetapkan callback jika response sukses dan error
+    xhr.onload = function () {
+      const responseJson = JSON.parse(this.responseText);
+      showResponseMessage(responseJson.message);
+      getBook();
+    };
+
+    xhr.onerror = function () {
+      showResponseMessage();
+    };
+
+    // Membuat PUT request dan menetapkan target URL
+    xhr.open('PUT', `${baseUrl}/edit/${book.id}`);
+
+    // Menetapkan properti Content-Type dan X-Auth-Token pada Header request
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('X-Auth-Token', '12345');
+
+    // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
+    xhr.send(JSON.stringify(book));
   };
 
   const removeBook = (bookId) => {
     // tuliskan kode di sini!
+
+    // Membuat instance dari XMLHttpRequest
+    const xhr = new XMLHttpRequest();
+
+    //menetapkan callback jika response sukses dan error
+    xhr.onload = function () {
+      const responseJson = JSON.parse(this.responseText);
+      showResponseMessage(responseJson.message);
+      getBook();
+    };
+
+    xhr.onerror = function () {
+      showResponseMessage();
+    };
+
+    // Membuat DELETE request dan menetapkan target URL
+    xhr.open('DELETE', `${baseUrl}/delete/${bookId}`);
+
+    // Menetapkan properti Content-Type dan X-Auth-Token pada Header request
+    xhr.setRequestHeader('X-Auth-Token', '12345');
+
+    // Mengirimkan request
+    xhr.send();
   };
 
 
-  
-  
-  
-  
+
+
+
+
   /*
       jangan ubah kode di bawah ini ya!
   */
@@ -48,7 +141,7 @@ function main() {
     buttons.forEach(button => {
       button.addEventListener('click', event => {
         const bookId = event.target.id;
-        
+
         removeBook(bookId);
       });
     });
@@ -72,7 +165,7 @@ function main() {
         title: inputBookTitle.value,
         author: inputBookAuthor.value
       };
-      
+
       insertBook(book);
     });
 
