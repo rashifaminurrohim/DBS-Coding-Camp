37f4cb6499fcbc58fcca8546c0b975126a7ecc13
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-04-16 22:57:17 +0700

    Fundamental Submission: Using Prettier for the formatter

--- FrontEnd-Web-Development-Fundamentals/submission/submission-final/notes-app/src/script/data/remote/notes.api.js
+++ FrontEnd-Web-Development-Fundamentals/submission/submission-final/notes-app/src/script/data/remote/notes.api.js
@@ -1,30 +1,32 @@
 export const notesApi = () => {
+  const BASE_URL = "https://notes-api.dicoding.dev/v2";
 
-  const BASE_URL = 'https://notes-api.dicoding.dev/v2';
-  
   const getNotes = async () => {
     try {
-      const options = { method: 'GET'}
+      const options = { method: "GET" };
       const response = await fetch(`${BASE_URL}/notes`, options);
       const responseJson = await response.json();
-  
+
       if (responseJson.error) {
-        return { success: false, message: responseJson.message};
+        return { success: false, message: responseJson.message };
       } else {
         return { success: true, data: responseJson.data };
       }
     } catch (error) {
-      return { success: false, message: error.message || 'Unknown error occurred' };
+      return {
+        success: false,
+        message: error.message || "Unknown error occurred",
+      };
     }
   };
 
   const createNote = async (title, body) => {
     try {
       const options = {
-        method: 'POST', 
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({title, body})
-      }
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title, body }),
+      };
       const response = await fetch(`${BASE_URL}/notes`, options);
       const responseJson = await response.json();
 
@@ -34,105 +36,130 @@ export const notesApi = () => {
         getNotes();
         return { success: true, data: responseJson.data };
       }
-
     } catch (error) {
-      return { success: false, message: error.message || 'Unknown error occurred' };
+      return {
+        success: false,
+        message: error.message || "Unknown error occurred",
+      };
     }
   };
 
   const deleteNote = async (noteId) => {
     try {
       const options = {
-        method: 'DELETE',
-      }
+        method: "DELETE",
+      };
       const response = await fetch(`${BASE_URL}/notes/${noteId}`, options);
       const responseJson = await response.json();
       getNotes();
       return { success: true, message: responseJson.message };
     } catch (error) {
-      return { success: false, message: error.message || 'Unknown error occurred' };
+      return {
+        success: false,
+        message: error.message || "Unknown error occurred",
+      };
     }
   };
 
   const archiveNote = async (noteId) => {
     try {
       const options = {
-        method: 'POST',
-        headers: {'Content-Type' : 'application/json'}
-      }
-      const response = await fetch(`${BASE_URL}/notes/${noteId}/archive`, options);
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      };
+      const response = await fetch(
+        `${BASE_URL}/notes/${noteId}/archive`,
+        options,
+      );
       const responseJson = await response.json();
       if (responseJson.error) {
-        return { success: false, message: responseJson.message }
+        return { success: false, message: responseJson.message };
       } else {
-        return { success: true, message: responseJson.message}
+        return { success: true, message: responseJson.message };
       }
     } catch (error) {
-      return { success: false, message: error.message || 'Unknown error occurred' };
+      return {
+        success: false,
+        message: error.message || "Unknown error occurred",
+      };
     }
-  
   };
 
   const getArchivedNotes = async () => {
-      try {
-        const options = { method: 'GET'}
-        const response = await fetch(`${BASE_URL}/notes/archived`, options);
-        const responseJson = await response.json();
-    
-        if (responseJson.error) {
-          return { success: false, message: responseJson.message};
-        } else {
-          return { success: true, data: responseJson.data };
-        }
-      } catch (error) {
-        return { success: false, message: error.message || 'Unknown error occurred' };
+    try {
+      const options = { method: "GET" };
+      const response = await fetch(`${BASE_URL}/notes/archived`, options);
+      const responseJson = await response.json();
+
+      if (responseJson.error) {
+        return { success: false, message: responseJson.message };
+      } else {
+        return { success: true, data: responseJson.data };
       }
-    
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message || "Unknown error occurred",
+      };
+    }
   };
 
   const unarchiveNote = async (noteId) => {
     try {
       const options = {
-        method: 'POST',
-        headers: {'Content-Type' : 'application/json'}
-      }
-      const response = await fetch(`${BASE_URL}/notes/${noteId}/unarchive`, options);
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      };
+      const response = await fetch(
+        `${BASE_URL}/notes/${noteId}/unarchive`,
+        options,
+      );
       const responseJson = await response.json();
       if (responseJson.error) {
-        return { success: false, message: responseJson.message }
+        return { success: false, message: responseJson.message };
       } else {
-        return { success: true, message: responseJson.message}
+        return { success: true, message: responseJson.message };
       }
     } catch (error) {
-      return { success: false, message: error.message || 'Unknown error occurred' };
+      return {
+        success: false,
+        message: error.message || "Unknown error occurred",
+      };
     }
   };
 
   const getAllNotes = async () => {
     try {
-      const [unarchived, archived] = await Promise.all([getNotes(), getArchivedNotes()]);
+      const [unarchived, archived] = await Promise.all([
+        getNotes(),
+        getArchivedNotes(),
+      ]);
       if (!unarchived.success || !archived.success) {
         return {
           success: false,
-          message: [unarchived.message, archived.message]
+          message: [unarchived.message, archived.message],
         };
       } else {
         return {
           success: true,
-          data: [...unarchived.data, ...archived.data]
+          data: [...unarchived.data, ...archived.data],
         };
       }
-
     } catch (error) {
       return {
         success: false,
-        message: error.message || 'Failed to fetch all notes',
+        message: error.message || "Failed to fetch all notes",
       };
     }
   };
 
-
-
-  return { getNotes, createNote, deleteNote, archiveNote, getArchivedNotes, unarchiveNote, getAllNotes };
+  return {
+    getNotes,
+    createNote,
+    deleteNote,
+    archiveNote,
+    getArchivedNotes,
+    unarchiveNote,
+    getAllNotes,
+  };
 };
-
