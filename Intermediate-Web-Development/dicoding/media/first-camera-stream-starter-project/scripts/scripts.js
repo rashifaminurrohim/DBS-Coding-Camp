ec250da408b41da5e0d805718da461f2f87c6d9b
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-04-29 20:48:14 +0700

    Media: MediaStream constraint settings

--- Intermediate-Web-Development/dicoding/media/first-camera-stream-starter-project/scripts/scripts.js
+++ Intermediate-Web-Development/dicoding/media/first-camera-stream-starter-project/scripts/scripts.js
@@ -2,12 +2,14 @@ let width = 320;
 let height = 0;
 
 let streaming = false;
+let currentStream;
 
 async function startup() {
   const cameraVideo = document.getElementById('camera-video');
   const cameraCanvas = document.getElementById('camera-canvas');
   const cameraTakeButton = document.getElementById('camera-take-button');
   const cameraOutputList = document.getElementById('camera-list-output');
+  const cameraListSelect = document.getElementById('camera-list-select');
 
   function populateTakenPicture(image) {
     cameraOutputList.innerHTML = `
@@ -17,9 +19,47 @@ async function startup() {
 
   async function getStream() {
     try {
-      return await navigator.mediaDevices.getUserMedia({
-        video: true,
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          deviceId: {
+            exact: !streaming ? undefined : cameraListSelect.value,
+          },
+          aspectRatio: 16 / 9,
+          width: {
+            min: 640,
+            max: 1920,
+            ideal: 1280
+          },
+          height: {
+            min: 480,
+            max: 1080,
+            ideal: 720
+          }
+        },
       });
+
+      // Show available camera after camera permission granted
+      await populateCameraList(stream);
+      
+      return stream;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function populateCameraList() {
+    try {
+      // Get all available webcam
+      const enumerateDevices = await navigator.mediaDevices.enumerateDevices();
+      const list = enumerateDevices.filter((device) => device.kind == 'videoinput');
+
+      cameraListSelect.innerHTML = list.reduce((accumulator, device, currentIndex) => {
+        return accumulator.concat(`
+            <option value="${device.deviceId}">
+              ${device.label || `Camera ${currentIndex + 1}`}
+            </option>  
+          `);
+      }, '');
     } catch (error) {
       throw error;
     }
@@ -37,6 +77,16 @@ async function startup() {
     context.drawImage(cameraVideo, 0, 0, width, height);
     
     return cameraCanvas.toDataURL('image/png');
+  }
+
+  function stopCurrentStream() {
+    if (!(currentStream instanceof MediaStream)) {
+      return;
+    }
+
+    currentStream.getTracks().forEach((track) => {
+      track.stop();
+    });
   }
 
   cameraTakeButton.addEventListener('click', () => {
@@ -59,10 +109,21 @@ async function startup() {
     streaming = true;
   });
 
+  cameraListSelect.addEventListener('change', async (event) => {
+    stopCurrentStream();
+
+    currentStream = await getStream();
+    cameraLaunch(currentStream);
+  })
+
   async function init() {
     try {
-      const stream = await getStream();
-      cameraLaunch(stream);
+      currentStream = await getStream();
+      cameraLaunch(currentStream);
+
+      currentStream.getVideoTracks().forEach((track) => {
+        console.log(track.getSettings());
+      });
     } catch (error) {
       console.error(error);
       alert('Error occured:', error.message);
