4fd8100d6264eeec6c253c3c461230b956a01d19
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-05-02 21:09:20 +0700

    Map and Location: City Care App Designing a Digital Map on Detail Page

--- Intermediate-Web-Development/dicoding/map-and-location/citycareapp-map-location-starter/src/scripts/config.js
+++ Intermediate-Web-Development/dicoding/map-and-location/citycareapp-map-location-starter/src/scripts/config.js
@@ -1,3 +1,5 @@
 export const ACCESS_TOKEN_KEY = 'accessToken';
 
 export const BASE_URL = 'https://citycare-api.dicoding.dev/v1';
+
+export const MAP_SERVICE_API_KEY = 'zBk7mH0I7aaYykOjeixR';
--- Intermediate-Web-Development/dicoding/map-and-location/citycareapp-map-location-starter/src/scripts/pages/report-detail/report-detail-page.js
+++ Intermediate-Web-Development/dicoding/map-and-location/citycareapp-map-location-starter/src/scripts/pages/report-detail/report-detail-page.js
@@ -11,11 +11,13 @@ import {
 import { createCarousel } from '../../utils';
 import ReportDetailPresenter from './report-detail-presenter';
 import { parseActivePathname } from '../../routes/url-parser';
+import Map from '../../utils/map';
 import * as CityCareAPI from '../../data/api';
 
 export default class ReportDetailPage {
   #presenter = null;
   #form = null;
+  #map = null;
 
   async render() {
     return `
@@ -66,6 +68,7 @@ export default class ReportDetailPage {
       description: report.description,
       damageLevel: report.damageLevel,
       evidenceImages: report.evidenceImages,
+      location: report.location,
       latitudeLocation: report.location.latitude,
       longitudeLocation: report.location.longitude,
       reporterName: report.reporter.name,
@@ -77,6 +80,14 @@ export default class ReportDetailPage {
 
     // Map
     await this.#presenter.showReportDetailMap();
+    if (this.#map) {
+      const reportCoordinate = [report.location.latitude, report.location.longitude];
+      const markerOptions = { alt: report.title };
+      const popupOptions = { content: report.title };
+      
+      this.#map.changeCamera(reportCoordinate);
+      this.#map.addMarker(reportCoordinate, markerOptions, popupOptions);
+    }
 
     // Actions buttons
     this.#presenter.showSaveButton();
@@ -122,6 +133,9 @@ export default class ReportDetailPage {
 
   async initialMap() {
     // TODO: map initialization
+    this.#map = await Map.build('#map', {
+      zoom:15,
+    });
   }
 
   #setupForm() {
