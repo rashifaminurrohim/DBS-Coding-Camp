e619d5981952d397f3e15837ba63e76da2f28926
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-05-10 10:51:05 +0700

    Javascript Background: City Care App study case push notification undone

--- Intermediate-Web-Development/dicoding/javascript-background-process/citycareapp-javascript-background-starter/src/scripts/index.js
+++ Intermediate-Web-Development/dicoding/javascript-background-process/citycareapp-javascript-background-starter/src/scripts/index.js
@@ -7,6 +7,7 @@ import 'leaflet/dist/leaflet.css';
 // Components
 import App from './pages/app';
 import Camera from './utils/camera';
+import { registerServiceWorker } from './utils';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const app = new App({
@@ -17,6 +18,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   });
   await app.renderPage();
 
+  await registerServiceWorker();
   window.addEventListener('hashchange', async () => {
     await app.renderPage();
 
--- Intermediate-Web-Development/dicoding/javascript-background-process/citycareapp-javascript-background-starter/src/scripts/pages/report-detail/report-detail-presenter.js
+++ Intermediate-Web-Development/dicoding/javascript-background-process/citycareapp-javascript-background-starter/src/scripts/pages/report-detail/report-detail-presenter.js
@@ -69,12 +69,46 @@ export default class ReportDetailPresenter {
         return;
       }
 
+      // No need to wait response
+      this.notifyReportOwner(response.data.id);
+
       this.#view.postNewCommentSuccessfully(response.message, response.data);
     } catch (error) {
       console.error('postNewComment: error:', error);
       this.#view.postNewCommentFailed(error.message);
     } finally {
       this.#view.hideSubmitLoadingButton();
+    }
+  }
+
+  async notifyMe() {
+    try {
+      const response = await this.#apiModel.sendReportToMeViaNotification(this.#reportId);
+
+      if (!response.ok) {
+        console.error('notifyMe: response:', response);
+        return;
+      }
+
+      console.log('notifyMe:', response.message);
+    } catch (error) {
+      console.error('notifyMe: error:', error);
+    }
+  }
+
+  async notifyReportOwner(commentId) {
+    try {
+      const response = await this.#apiModel.sendCommentToReportOwnerViaNotification(
+        this.#reportId,
+        commentId,
+      );
+      if (!response.ok) {
+        console.error('notifyReportOwner: response:', response);
+        return;
+      }
+      console.log('notifyReportOwner:', response.message);
+    } catch (error) {
+      console.error('notifyReportOwner: error:', error);
     }
   }
 
