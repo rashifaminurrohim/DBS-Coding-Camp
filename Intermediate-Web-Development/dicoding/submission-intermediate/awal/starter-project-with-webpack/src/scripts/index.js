d773d1542212151acedafb8369f077b08b4b90f0
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-05-23 15:36:24 +0700

    Submission Intermediate: pwa feature done

--- Intermediate-Web-Development/dicoding/submission-intermediate/awal/starter-project-with-webpack/src/scripts/index.js
+++ Intermediate-Web-Development/dicoding/submission-intermediate/awal/starter-project-with-webpack/src/scripts/index.js
@@ -1,5 +1,6 @@
 // CSS imports
 import '../styles/styles.css';
+import '../styles/responsive.css';
 import 'leaflet/dist/leaflet.css';
 
 // Components
--- Intermediate-Web-Development/dicoding/submission-intermediate/awal/starter-project-with-webpack/src/scripts/sw.js
+++ Intermediate-Web-Development/dicoding/submission-intermediate/awal/starter-project-with-webpack/src/scripts/sw.js
@@ -1,3 +1,44 @@
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { NetworkFirst, CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import CONFIG from './config';
+
+// Do precaching
+const manifest = self.__WB_MANIFEST;
+precacheAndRoute(manifest);
+
+// Runtime caching
+registerRoute(
+  ({ request, url }) => {
+    const baseUrl = new URL(CONFIG.BASE_URL);
+    return baseUrl.origin === url.origin && request.destination !== 'image';
+  },
+  new NetworkFirst({
+    cacheName: 'story-api',
+  }),
+);
+
+registerRoute(
+  ({ request, url }) => {
+    const baseUrl = new URL(CONFIG.BASE_URL);
+    return baseUrl.origin === url.origin && request.destination === 'image';
+  },
+  new StaleWhileRevalidate({
+    cacheName: 'story-api-image',
+  }),
+);
+
+registerRoute(
+  ({ url }) => {
+    return url.origin.includes('maptiler');
+  },
+  new CacheFirst({
+    cacheName: 'maptiler-api',
+  }),
+);
+
+
 self.addEventListener('push', (event) => {
   console.log('[Service worker] pushing...');
 
