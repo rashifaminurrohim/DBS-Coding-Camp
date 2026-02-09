bfa3d878dbed73fef6b4b9e9f2b6026845171bbd
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2026-02-09 13:30:15 +0700

    Submission Open Music API v2: authentication implemented

--- Javascript-BackEnd-Fundamental/open-music-api-backend/src/api/songs/routes.js
+++ Javascript-BackEnd-Fundamental/open-music-api-backend/src/api/songs/routes.js
@@ -5,28 +5,28 @@ const routes = (handler) => [
     path: '/songs',
     handler: handler.postSongHandler,
   },
-
+  
   // Ambil semua lagu (nanti bisa ditambah query ?title & ?performer)
   {
     method: 'GET',
     path: '/songs',
     handler: handler.getSongsHandler,
   },
-
+  
   // Detail lagu by id
   {
     method: 'GET',
     path: '/songs/{id}',
     handler: handler.getSongByIdHandler,
   },
-
+  
   // Update lagu by id
   {
     method: 'PUT',
     path: '/songs/{id}',
     handler: handler.putSongByIdHandler,
   },
-
+  
   // Hapus lagu by id
   {
     method: 'DELETE',
--- Javascript-BackEnd-Fundamental/open-music-api-backend/src/server.js
+++ Javascript-BackEnd-Fundamental/open-music-api-backend/src/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+
 const albums = require('./api/albums');
 const songs = require('./api/songs');
 
@@ -14,12 +16,18 @@ const users = require('./api/users');
 const UsersService = require('./services/postgres/users/UsersService');
 const UsersValidator = require('./validator/users');
 
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/authentications/AuthenticationsService');
+const AuthenticationsValidator = require('./validator/authentications');
+const TokenManager = require('./tokenize/TokenManager');
+
 
 
 const init = async () => {
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
-  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -29,6 +37,28 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('openmusicapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
@@ -51,6 +81,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        usersService,
+        authenticationsService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
